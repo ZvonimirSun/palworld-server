a94f07e2ab72e4041080cdfe6ff2024ba7337720
ZvonimirSun <hi@iszy.xyz>
2024-01-22 22:28:30 +0800

    edit Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ From steamcmd/steamcmd
 
 RUN groupadd -r steam && useradd -r -g steam steam
 
-USER steam
-
 # Copy our shell script to docker container.
 # This shell script will be used to download gamefiles
 # and configure our gameserver.
--- download_and_run_gameserver.sh
+++ download_and_run_gameserver.sh
@@ -6,6 +6,7 @@
 # Run SteamCMD to download the game files using app ID - 2394010 ( Palworld server )
 steamcmd +force_install_dir /home/game_server +login anonymous +app_update 2394010 validate +quit
 
+chown steam:steam /home/game_server -Rf
+su steam
 cd /home/game_server
-
 ./PalServer.sh
